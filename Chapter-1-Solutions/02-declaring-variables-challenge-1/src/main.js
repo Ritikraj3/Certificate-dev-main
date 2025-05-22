755a45f9b03d131ed0152246d59a11c450a11104
Ritikraj3 <ritikraj5776@gmail.com>
2025-05-22 18:03:24 +0530

    splved q2

---
+++ Chapter-1-Solutions/02-declaring-variables-challenge-1/src/main.js
@@ -0,0 +1,33 @@
+// Complete the challenge requirements here
+
+//1.
+const Javascript = "King"  
+console.log(Javascript)    
+
+console.log(typeof Javascript) // string
+
+//2.
+console.log(Javascript.includes("HTML")) //false
+
+//3.
+let age = 12
+console.log(age)
+age=13
+console.log(age)
+
+//4.
+const isPreppingForExam = true
+console.log(isPreppingForExam)
+
+//5.
+const isOlderThan8 = age > 8
+console.log(isOlderThan8) //boolean
+
+//6.
+var thisVarHasNoValue 
+
+console.log(thisVarHasNoValue) //undefined
+
+
+
+
